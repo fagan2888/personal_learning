1ce9cf697ad9712e7d82adc928fc12e9f3d11041
mariaathena <me@mariaathena.com>
2017-06-23 17:07:46 +0100

    complete week1 hw

--- ucberkeley_cs61a/lab00/lab00.py
+++ ucberkeley_cs61a/lab00/lab00.py
@@ -1,8 +1,8 @@
-def twenty_seventeen():
-    """Come up with the most creative expression that evaluates to 2017,
+def twenty_sixteen():
+    """Come up with the most creative expression that evaluates to 2016,
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_seventeen()
-    2017
+    >>> twenty_sixteen()
+    2016
     """
-    return ______
+    return 2015 + 1
